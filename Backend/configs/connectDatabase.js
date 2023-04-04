3dc27a173ba9f67105b0343599cbc1dad74291d5
Adil khan <111215638+Adil-khan-007@users.noreply.github.com>
2023-04-05 03:24:48 +0530

    Delete Backend directory

--- Backend/configs/connectDatabase.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-const connectDatabase =  async (req,res)=>{
-    return mongoose.connect(process.env.MONGODB);
-}
-
-module.exports = connectDatabase;
